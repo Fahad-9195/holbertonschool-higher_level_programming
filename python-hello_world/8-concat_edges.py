f1461a295bce242d10269ec5cd68a655e7e1729a
root <root@ip-10-42-18-31.ec2.internal>
2025-11-24 18:59:20 +0000

    Fix pycodestyle E501 in 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = (
-    "Python is an interpreted, interactive, object-oriented programming language "
-    "that combines remarkable power with very clear syntax"
-)
+    "Python is an interpreted, interactive, object-oriented programming language " "that combines remarkable power with very clear syntax")
 print(str[39:66] + str[75:79] + str[:6])
