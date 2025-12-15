6c228760ae9bc0883e0a7838e7224f249ca0871e
Fahad-9195 <fahad-9195@hotmail.com>
2025-12-15 13:31:00 +0300

    Clean up comments in integer_validator method
    Removed redundant comments in integer_validator method.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,9 +12,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validate that value is a positive integer.
-        """
+        """Validate that value is a positive integer."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
