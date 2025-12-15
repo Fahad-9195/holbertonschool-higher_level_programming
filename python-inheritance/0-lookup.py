31d998cfc0501c58abc80a9ef9b5ee877979a134
Fahad-9195 <fahad-9195@hotmail.com>
2025-12-15 13:00:14 +0300

    Refactor docstring in lookup function
    Removed summary docstring and added a clearer description.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-"""
-    _summary_
-"""
+"""Returns the list of available attributes and methods of an object."""
+
+def lookup(obj):
+    """Return a list of attributes and methods of an object."""
+    return dir(obj)
